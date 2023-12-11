ebf6f30e257ff15d45bcc26885857b13d965ee0e
tecstaffing-dev <85949527+tecstaffing-dev@users.noreply.github.com>
2023-12-11 13:40:26 -0600

    Update kiosksetup.sh

--- kiosksetup.sh
+++ kiosksetup.sh
@@ -13,6 +13,15 @@ if [ "$(id -u)" -ne 0 ]; then
         exit 1
 fi
 
+# setup autologin
+systemctl set-default multi-user.target
+ln -fs /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
+cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin $USER --noclear %I \$TERM
+EOF
+
 #Change Hostname
 old=$(cat /etc/hostname)
 new=$(tr -dc 'A-Z0-9' < /dev/urandom | head -c12)
@@ -30,7 +39,16 @@ apt autoremove -y
 # backup original autostart file
 mv /etc/xdg/openbox/autostart /etc/xdg/openbox/autostart.bak
 # copy customized autostart file from host
-curl -o /etc/xdg/openbox/autostart http://192.168.1.165/autostart
+curl -o /etc/xdg/openbox/autostart https://raw.githubusercontent.com/tecstaffing-dev/kiosksetup/main/autostart
+# mark executable
+chmod +X /etc/xdg/openbox/autostart
+# pull firefox.sh
+curl -o /home/pi/firefox.sh https://raw.githubusercontent.com/tecstaffing-dev/kiosksetup/main/firefox.sh
+# mark executable
+chmod +X /home/pi/firefox.sh
+# owned by pi user
+chown -R pi:pi /home/pi/firefox.sh
+# Create autostart for startx
 echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx --" > /home/pi/.bash_profile
 chown -R pi:pi /home/pi/.bash_profile
 echo "Script is complete... rebooting in 10 seconds."
